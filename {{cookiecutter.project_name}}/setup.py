b5fc115431ec286f10a9eea43bb87416b2a95033
xiongxianzhu <929679459@qq.com>
2018-04-10 18:36:03 +0800

    update

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+
+__version__ = '0.1'
+
+
+setup(
+    name='{{cookiecutter.app_name}}',
+    version=__version__,
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'flask',
+        'flask-sqlalchemy',
+        'flask-restful',
+        'flask-migrate',
+    ],
+    entry_points={
+        'console_scripts': [
+            '{{cookiecutter.app_name}} = {{cookiecutter.app_name}}.manage:cli'
+        ]
+    }
+)
